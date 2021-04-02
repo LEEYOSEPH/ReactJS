55be8d8171bd7af9aacadf463cfe110e460e76ef
LEEYOSEPH <laksmi0628@gmail.com>
2021-04-02 15:00:28 +0900

    #11 임의 메서드 만들기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -4,6 +4,25 @@ class EventPractice extends Component {
   state = {
     message: '',
   };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: '',
+    });
+  }
   render() {
     return (
       <div>
@@ -13,12 +32,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력 하세요"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
         />
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
