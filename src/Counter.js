375684f40dfde434be3c1c577eb0f97a31a993c9
LEEYOSEPH <laksmi0628@gmail.com>
2021-04-02 12:27:22 +0900

    #03 state객체에 여러 값 넣기

--- src/Counter.js
+++ src/Counter.js
@@ -6,13 +6,15 @@ class Counter extends Component {
     //state의 초기값 설정
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
   render() {
-    const { number } = this.state; // state를 조회 할 때는 this.state로 조회 한다.
+    const { number, fixedNumber } = this.state; // state를 조회 할 때는 this.state로 조회 한다.
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button
           // onClick을 통해 버튼이 클릭 되었을 때 호출할 함수를 지정
           onClick={() => {
