c92db929742b1305b0a1ca99729bf8b5e0394d34
LEEYOSEPH <laksmi0628@gmail.com>
2021-04-06 13:55:20 +0900

    #210406 컴포넌트 태그사이의 내용을 보여주는 props(children)

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const MyComponent = (props) => {
-  return <div>안녕하세요, 제이름은 {props.name}이에요</div>;
+  return (
+    <div>
+      안녕하세요, 제이름은 {props.name}이에요 children 값은 {props.children}
+      이에요
+    </div>
+  );
 };
 
 MyComponent.defaultProps = {
