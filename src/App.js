1b1473feeadfe8144ff657d9e7ed0b70a6801171
LEEYOSEPH <laksmi0628@gmail.com>
2021-04-13 17:12:08 +0900

    #2104013 immer  사용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,97 @@
-import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import React, { useCallback, useRef, useState } from 'react';
+import produce from 'immer';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: '', username: '' });
+  const [data, setData] = useState({
+    array: [],
+    uselessValye: null,
+  });
+
+  //input 수정을 위한 함수
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setForm(
+        produce(form, (draft) => {
+          draft[name] = value;
+        })
+      );
+    },
+    [form]
+  );
+
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      // array에 새 항목 등록
+      setData(
+        produce(data, (draft) => {
+          draft.array.push(info);
+        })
+      );
+
+      //form 초기화
+      setForm({
+        name: ``,
+        username: ``,
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
+
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    (id) => {
+      setData(
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
+    },
+    [data]
+  );
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="ID"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="Name"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">submit</button>
+      </form>
       <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.ScrollToBottom}>down</button>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username}({info.name})
+            </li>
+          ))}
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
