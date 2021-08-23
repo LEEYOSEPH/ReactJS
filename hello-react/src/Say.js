46e20f55dc78a05d39c6649f6e87592d2a92180a
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-23 16:19:45 +0900

    feat: useState 여러번 사용

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -4,11 +4,15 @@ const Say = () => {
   const [message, setMessage] = useState("");
   const onClickEnter = () => setMessage("hello");
   const onClickLeave = () => setMessage("buy");
+  const [color, setColor] = useState("black");
   return (
     <div>
       <button onClick={onClickEnter}>enter</button>
       <button onClick={onClickLeave}>Leave</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        red
+      </button>
     </div>
   );
 };
