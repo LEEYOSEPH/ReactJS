c2ff4a9e4043d0675fd737ce239b84f6b5b4840a
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-24 14:20:58 +0900

    feat: 컴포넌트 추가 제거

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: "A" },
+    { id: 2, text: "B" },
+    { id: 3, text: "C" },
+    { id: 4, text: "D" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      tet: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>;
+    </>
+  );
 };
 
 export default IterationSample;
