78f63781b0ec09aac174e5c5f901765bcec0ff4f
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-24 16:08:36 +0900

    feat: useEffect

---
+++ hooks-tutorial/src/Info.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeNickName = (e) => {
+    setNickname(e.target.value);
+  };
+  useEffect(() => {
+    console.log(name);
+    return () => {
+      console.log(name);
+    };
+  }, [name]);
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickName} />
+      </div>
+      <div>
+        <div>이름 : {name}</div>
+        <div>닉네임 : {nickname}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
