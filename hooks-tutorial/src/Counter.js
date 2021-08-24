b37bde0eccd6b0796aabfdb1131747bf7b517ecd
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-24 16:30:01 +0900

    feat: useReducer

--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
-
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
   return (
     <div>
-      <p>
-        current count value is <b>{value}</b>
-      </p>
-      <button onClick={() => setValue(value + 1)}>+1</button>
-      <button onClick={() => setValue(value - 1)}>-1</button>
+      <p>현재 카운터 값은 {state.value}</p>
+      <butt onClick={() => dispatch({ type: "INCREMENT" })}>+1</butt>
+      <butt onClick={() => dispatch({ type: "DECREMENT" })}>-1</butt>
     </div>
   );
 };
