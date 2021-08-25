163fad7770be270f69a54919200e2a5288fab5e2
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-25 17:09:36 +0900

    feat: 기능구현 1

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 import TodoTemplate from './TodoTemplate';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'hello',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'two',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'three',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
