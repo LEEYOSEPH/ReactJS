da64f08b91a1d51fa19cfa378ae470a2d85afefa
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-25 16:53:21 +0900

    feat: create TodoListItem & TodoList

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoInsert from './TodoInsert';
+import TodoList from './TodoList';
 import TodoTemplate from './TodoTemplate';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
