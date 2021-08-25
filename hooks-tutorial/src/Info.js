df6aec8330bf34a68d9a488331bf0203be09b2d1
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-25 15:08:18 +0900

    feat: 커스텀 Hooks

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer, useState } from "react";
-
+import { useInputs } from "./useInputs";
 function reducer(state, action) {
   return {
     ...state,
@@ -8,7 +8,7 @@ function reducer(state, action) {
 }
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useInputs({
     name: "",
     nickname: "",
   });
