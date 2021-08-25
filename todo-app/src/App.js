086d45760e5926f83e2ab7d72fff4db7a45014e0
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-25 17:27:22 +0900

    feat: Remove 기능 추가

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -35,10 +35,17 @@ const App = () => {
     setTodos(todos.concat(todo));
     nextId.current += 1;
   });
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.fillter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} />
     </TodoTemplate>
   );
 };
