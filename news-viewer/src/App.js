38b7561c3286b087b05fe2545cb2aca25e1aab67
YOSEPHLEE <laksmi0628@gmail.com>
2021-09-02 16:05:39 +0900

    feat: async/await 적용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -4,12 +4,15 @@ import axios from '../node_modules/axios/index';
 
 const App = () => {
   const [data, setDate] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setDate(response.data);
-      });
+  const onClick = async () => {
+    try {
+      const response = await aioxs.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
+      setDate(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
