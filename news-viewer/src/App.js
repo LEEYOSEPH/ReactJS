cb946295f0a3951880e905da23bb9add3403a7bf
YOSEPHLEE <laksmi0628@gmail.com>
2021-09-02 16:04:02 +0900

    feat: create news-viewer

---
+++ news-viewer/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import aioxs from 'axios';
+import axios from '../node_modules/axios/index';
+
+const App = () => {
+  const [data, setDate] = useState(null);
+  const onClick = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        setDate(response.data);
+      });
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
