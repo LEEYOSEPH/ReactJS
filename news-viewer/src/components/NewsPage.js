6e5bec9ad9bd2e146bba8ba554d577dd3252779b
YOSEPHLEE <laksmi0628@gmail.com>
2021-09-02 17:10:15 +0900

    feat: react route 적용

---
+++ news-viewer/src/components/NewsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Categories from './Categories';
+import NewsList from './NewsList';
+const NewsPage = ({ match }) => {
+  //카테고리가 선택되지 않으면 기본값 all 사용
+  const category = match.params.category || 'all';
+  return (
+    <>
+      <Categories />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default NewsPage;
