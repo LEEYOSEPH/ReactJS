82e6213c977fe4e42fc5a0f32fce3d40dac44dbc
YOSEPHLEE <laksmi0628@gmail.com>
2021-09-10 14:00:57 +0900

    feat: install proxy

--- boilerplate/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 const LandingPage = () => {
+  useEffect(() => {
+    axios.get('/api/hello').then((response) => {
+      console.log(response);
+    });
+  }, []);
   return <div>LandingPage</div>;
 };
 
