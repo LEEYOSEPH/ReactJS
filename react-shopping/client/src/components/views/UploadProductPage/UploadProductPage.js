2479607fae3bb41c52eeac2cbf51456a689cda0e
YOSEPHLEE <laksmi0628@gmail.com>
2021-09-15 11:21:37 +0900

    feat: dropzone 생성

--- react-shopping/client/src/components/views/UploadProductPage/UploadProductPage.js
+++ react-shopping/client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, Input } from 'antd';
+import FileUpload from '../../utils/FileUpload';
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -40,6 +41,7 @@ const UploadProductPage = () => {
       </div>
       <Form>
         {/* dropzone */}
+        <FileUpload />
         <br />
         <br />
         <label>이름</label>
