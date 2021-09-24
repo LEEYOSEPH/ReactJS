6c8ef6743438d5bf1d458647d2c8a3b153340ee8
YOSEPHLEE <laksmi0628@gmail.com>
2021-09-24 10:45:45 +0900

    feat: productImage

--- react-shopping/client/src/components/views/DetailProductPage/DetailProductPage.js
+++ react-shopping/client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import ProductImage from './Sections/ProductImage';
+import ProductInfo from './Sections/ProductInfo';
+import { Row, Col } from 'antd';
 
 const DetailProductPage = (props) => {
   const productId = props.match.params.productId;
+  const [product, setProduct] = useState({});
 
   useEffect(() => {
     axios
@@ -10,12 +14,29 @@ const DetailProductPage = (props) => {
       .then((response) => {
         if (response.data.success) {
           console.log(response.data);
+          setProduct(response.data.product[0]);
         } else {
           alert('성공');
         }
       });
   }, []);
-  return <div>DetailProductPage</div>;
+  return (
+    <div style={{ width: '100%', padding: '3rem 4rem' }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h1>{product.title}</h1>
+      </div>
+      <br />
+      <Row gutter={[16, 16]}>
+        <Col lg={12} sm={24}>
+          <ProductImage detail={product} />
+        </Col>
+
+        <Col lg={12} sm={24}>
+          <ProductInfo />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default DetailProductPage;
